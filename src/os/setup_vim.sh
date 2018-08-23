2ef2985c739b9ca1e5c18f3b4cb9222ec5ad3c70
skylerto <skylerclayne@gmail.com>
2018-08-23 16:24:03 -0400

    python
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- src/os/setup_vim.sh
+++ src/os/setup_vim.sh
@@ -7,6 +7,9 @@ main() {
   declare -r VUNDLE_DIR="$HOME/.vim/plugins/Vundle.vim"
   declare -r VUNDLE_GIT_REPO_URL="https://github.com/gmarik/Vundle.vim.git"
 
+  sudo pip3 install neovim
+  sudo pip2 install neovim
+
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   printf '\n' | nvim +PlugInstall +qall
 
